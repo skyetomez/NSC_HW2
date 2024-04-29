505b9f3252f98f364f8abee6e0651e291ea49d29
Sky Tomez <sthom215@jhu.edu>
2024-04-29 13:17:51 -0400

    fixed prob 2 problem 1 has been fixed i.e. the optimization loop works as well as parts of problem 2

--- sthom215_problem_1_ex_2.py
+++ sthom215_problem_1_ex_2.py
@@ -1,15 +1,13 @@
 import numpy as np
 import scipy.signal as sig
 import matplotlib.pyplot as plt
-import seaborn as sns
-import polars as pl
+from typing import Tuple
 from numpy.typing import NDArray
-
-RNG = np.random.default_rng(1337)
 
 
 class Experiment:
     def __init__(self, *args: int, **kwargs) -> None:
+        self.RNG = np.random.default_rng(1337)
         self.stimuli = (args, kwargs)
         # self.kwargs = kwargs
 
@@ -29,38 +27,30 @@ class Experiment:
             sigma = 1
 
         if len(args) > 1:
-            self.n_trials = args[0]
-            self.n_neurons = args[1]
-            size = (self.n_trials, self.n_neurons)
-            self._stimuli = 2 * RNG.normal(mu, sigma, size)
+            self.n_neurons = args[0]
+            self.n_trials = args[1]
+            size = (self.n_neurons, self.n_trials)
+            self._stimuli = 2 * self.RNG.normal(mu, sigma, size)
         else:
             self.n_trials = () if args == None else args[0]
             self.n_neurons = 1
-            self._stimuli = 2*RNG.normal(mu, sigma, (self.n_trials))
+            self._stimuli = 2*self.RNG.normal(mu, sigma, (self.n_trials))
 
     def get_tuning_curve(self, N: int):
         return sig.windows.gaussian(N, 5) * np.cos(2*np.pi*np.arange(N)/10)
 
     def activity(self):
-        if self.n_neurons == 1:  # () integers
-            tcurve = self.get_tuning_curve(self.n_trials)
-            self.intensity = np.exp(self._stimuli @ tcurve)
-            return self.intensity
-        elif self.n_neurons > 1:
-            tmat = np.vstack([self.get_tuning_curve(self.n_neurons)
-                             for _ in range(self.n_trials)])
-            self.intensity = np.exp(self._stimuli @ tmat)
-            return self.intensity
-        else:
-            print('Invalid number of neurons')
+        tcurve = self.get_tuning_curve(self.n_trials)
+        self.intensity = np.exp(self._stimuli @ tcurve)
+        return self.intensity
 
 
-class Neruon(Experiment):
+class Neuron(Experiment):
     def __init__(self, *args: int, **kwargs) -> None:
         super().__init__(*args, **kwargs)
 
     def spiking(self):
         if self.n_trials > 1:
-            return RNG.poisson(lam=self.activity(), size=(self.n_trials,))
+            return self.RNG.poisson(lam=self.activity(), size=(self.n_trials,))
         else:
-            return RNG.poisson(lam=self.activity())
+            return self.RNG.poisson(lam=self.activity())
